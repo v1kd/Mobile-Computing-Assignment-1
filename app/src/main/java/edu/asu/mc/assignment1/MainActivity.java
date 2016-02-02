a293be5ce15b90b3f5f633eccc5a76161ad87fa0
Vikranth <vickydoosa@github.com>
2016-02-01 18:45:14 -0700

    Refactors code to remove unused imports

--- app/src/main/java/edu/asu/mc/assignment1/MainActivity.java
+++ app/src/main/java/edu/asu/mc/assignment1/MainActivity.java
@@ -2,14 +2,11 @@ package edu.asu.mc.assignment1;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
